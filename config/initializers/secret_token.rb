b8d7c24c4fcb1516ff78c2d50c5a3abb9c1c546e
Jose Infante <jminfante_007@wgl-10-105-78-76.p.fiu.edu>
2012-09-20 21:37:27 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuggestionBox::Application.config.secret_token = '332f27529bbe952156964d1a692c1cfacbc39011e37d454afde48cc0b6ec44885cb8cbe1df1d3a6c6f35687b13fee32ac0c5f6056ad5b3523156875bb31dee83'
